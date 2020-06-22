4052954e768006271f557b661691bee749932dbf
Nicolas-alt <a10100105555@gmail.com>
2020-06-21 19:56:35 -0500

    [Feat] Inicio de sesión

--- LogBook/LogBook/urls.py
+++ LogBook/LogBook/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('home', views.index, name = 'index'),
+    path('usuarios/login', views.login_form_view, name = 'login'),
+    path('usuarios/logout', views.log_out, name= 'logout'),
+    path('usuarios/recuperarCuenta', views.restoreAccount, name= 'recuperarCuenta'),
+    path('usuarios/registro', views.registro, name= 'registro'),
+    path('usuarios/dashboard', login_required(views.dashboard_view), name = 'dashboard'),
     path('admin/', admin.site.urls),
 ]
-
--- LogBook/LogBook/views.py
+++ LogBook/LogBook/views.py
@@ -1,7 +1,42 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib import messages
 
 def index(request):
     return render(request,'index.html',{
-        #Context
     })
+
+def login_form_view(request):
+    if request.method == 'POST':
+        username  = request.POST.get('user')
+        password = request.POST.get('password')
+        
+        userData = authenticate(username = username, password = password)
+
+        if userData:
+            login(request,userData)
+            messages.success(request, 'Bienvenido {}'.format(userData.username))
+            return redirect('dashboard')
+        else:
+            messages.error(request,'Ups! Algo salió mal')
+            
+    return render(request, 'loggin.html')
+
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+
+def log_out(request):
+    logout(request)
+    return redirect('login')
+
+def registro(request):
+    return render(request, 'form.html',{
+
+     })
+
+def restoreAccount(request):
+    return render(request, 'recuperarCuenta.html')
